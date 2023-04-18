0c2cdcb3b826ff866df05231e422be43aec85751
Tejas <60872437+tejasraibagi26@users.noreply.github.com>
2023-04-18 14:29:52 -0400

    Added API versioning

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,13 +1,14 @@
 
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_cors import CORS
 
 from api import db
 
 _db = db.connect_to_db()
 app = Flask(__name__)
+
+v1 = Blueprint('v1', __name__, url_prefix='/api/v1')
+
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-
 
 from api import routes
